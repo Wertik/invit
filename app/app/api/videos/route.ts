1a680b957edc8b6ee655aa06c3e6634d974e2276
Wertik <jregvx@gmail.com>
2025-01-06 14:18:41 +0100

    fix: move ignore, add keep, ignore system files in video search

--- app/app/api/videos/route.ts
+++ app/app/api/videos/route.ts
@@ -3,7 +3,9 @@ import fs from "fs/promises"
 import path from "path"
 
 export async function GET() {
-	const files = await fs.readdir('public/videos')
+	const files = (await fs.readdir('public/videos')).filter((file) => {
+		return !file.startsWith(".");
+	})
 
 	return NextResponse.json(files.map((file) => path.parse(file).name)
 		.filter((file, index, array) => {
